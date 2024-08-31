1005326465f0dec39051321070296f7c2583bbb5
hagikenn <146914983+hagikenn@users.noreply.github.com>
2024-08-31 12:12:55 +0900

    kakuninn

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,108 +1,123 @@
-#include "Audio.h"
-#include "AxisIndicator.h"
-#include "DirectXCommon.h"
-#include "GameScene.h"
-#include "ImGuiManager.h"
-#include "PrimitiveDrawer.h"
-#include "TextureManager.h"
-#include "WinApp.h"
+#include <DirectXMath.h>
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <windows.h>
+#include <wrl/client.h>
 
-// Windowsアプリでのエントリーポイント(main関数)
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	WinApp* win = nullptr;
-	DirectXCommon* dxCommon = nullptr;
-	// 汎用機能
-	Input* input = nullptr;
-	Audio* audio = nullptr;
-	AxisIndicator* axisIndicator = nullptr;
-	PrimitiveDrawer* primitiveDrawer = nullptr;
-	GameScene* gameScene = nullptr;
+using namespace DirectX;
+using Microsoft::WRL::ComPtr;
 
-	// ゲームウィンドウの作成
-	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"LE2D_18_ハギワラ_ケンタ_AL3");
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	switch (message) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	default:
+		return DefWindowProc(hwnd, message, wParam, lParam);
+	}
+}
 
-	// DirectX初期化処理
-	dxCommon = DirectXCommon::GetInstance();
-	dxCommon->Initialize(win);
+HWND InitWindow(HINSTANCE hInstance, int nCmdShow) {
+	WNDCLASSEX wcex = {0};
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.hInstance = hInstance;
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszClassName = L"DirectXWindowClass";
 
-#pragma region 汎用機能初期化
-	// ImGuiの初期化
-	ImGuiManager* imguiManager = ImGuiManager::GetInstance();
-	imguiManager->Initialize(win, dxCommon);
+	if (!RegisterClassEx(&wcex))
+		return nullptr;
 
-	// 入力の初期化
-	input = Input::GetInstance();
-	input->Initialize();
+	RECT rc = {0, 0, 800, 600};
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
 
-	// オーディオの初期化
-	audio = Audio::GetInstance();
-	audio->Initialize();
+	HWND hwnd =
+	    CreateWindow(L"DirectXWindowClass", L"DirectX 11 Game", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, hInstance, nullptr);
 
-	// テクスチャマネージャの初期化
-	TextureManager::GetInstance()->Initialize(dxCommon->GetDevice());
-	TextureManager::Load("white1x1.png");
+	if (hwnd) {
+		ShowWindow(hwnd, nCmdShow);
+	}
+	return hwnd;
+}
 
-	// スプライト静的初期化
-	Sprite::StaticInitialize(dxCommon->GetDevice(), WinApp::kWindowWidth, WinApp::kWindowHeight);
+ComPtr<ID3D11Device> device;
+ComPtr<ID3D11DeviceContext> context;
+ComPtr<IDXGISwapChain> swapChain;
+ComPtr<ID3D11RenderTargetView> renderTargetView;
 
-	// 3Dモデル静的初期化
-	Model::StaticInitialize();
+HRESULT InitDirect3D(HWND hwnd) {
+	DXGI_SWAP_CHAIN_DESC sd = {};
+	sd.BufferCount = 1;
+	sd.BufferDesc.Width = 800;
+	sd.BufferDesc.Height = 600;
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.OutputWindow = hwnd;
+	sd.SampleDesc.Count = 1;
+	sd.Windowed = TRUE;
 
-	// 軸方向表示初期化
-	axisIndicator = AxisIndicator::GetInstance();
-	axisIndicator->Initialize();
+	HRESULT hr = D3D11CreateDeviceAndSwapChain(
+	    nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, 0, nullptr, 0, D3D11_SDK_VERSION, &sd, swapChain.GetAddressOf(), device.GetAddressOf(), nullptr, context.GetAddressOf());
 
-	primitiveDrawer = PrimitiveDrawer::GetInstance();
-	primitiveDrawer->Initialize();
-#pragma endregion
-
-	// ゲームシーンの初期化
-	gameScene = new GameScene();
-	gameScene->Initialize();
-
-	// メインループ
-	while (true) {
-		// メッセージ処理
-		if (win->ProcessMessage()) {
-			break;
-		}
-
-		// ImGui受付開始
-		imguiManager->Begin();
-		// 入力関連の毎フレーム処理
-		input->Update();
-		// ゲームシーンの毎フレーム処理
-		gameScene->Update();
-		// 軸表示の更新
-		axisIndicator->Update();
-		// ImGui受付終了
-		imguiManager->End();
-
-		// 描画開始
-		dxCommon->PreDraw();
-		// ゲームシーンの描画
-		gameScene->Draw();
-		// 軸表示の描画
-		axisIndicator->Draw();
-		// プリミティブ描画のリセット
-		primitiveDrawer->Reset();
-		// ImGui描画
-		imguiManager->Draw();
-		// 描画終了
-		dxCommon->PostDraw();
+	if (FAILED(hr)) {
+		return hr;
 	}
 
-	// 各種解放
-	delete gameScene;
-	// 3Dモデル解放
-	Model::StaticFinalize();
-	audio->Finalize();
-	// ImGui解放
-	imguiManager->Finalize();
+	ComPtr<ID3D11Texture2D> backBuffer;
+	hr = swapChain->GetBuffer(0, IID_PPV_ARGS(backBuffer.GetAddressOf()));
+	if (FAILED(hr))
+		return hr;
 
-	// ゲームウィンドウの破棄
-	win->TerminateGameWindow();
+	hr = device->CreateRenderTargetView(backBuffer.Get(), nullptr, renderTargetView.GetAddressOf());
+	if (FAILED(hr))
+		return hr;
 
-	return 0;
+	context->OMSetRenderTargets(1, renderTargetView.GetAddressOf(), nullptr);
+
+	// ビューポート設定
+	D3D11_VIEWPORT vp = {};
+	vp.Width = 800.0f;
+	vp.Height = 600.0f;
+	vp.MinDepth = 0.0f;
+	vp.MaxDepth = 1.0f;
+	vp.TopLeftX = 0.0f;
+	vp.TopLeftY = 0.0f;
+	context->RSSetViewports(1, &vp);
+
+	return S_OK;
+}
+
+void Render() {
+	const float clearColor[4] = {0.0f, 0.2f, 0.4f, 1.0f};
+	context->ClearRenderTargetView(renderTargetView.Get(), clearColor);
+
+	// ここに描画コードを追加します
+
+	swapChain->Present(1, 0);
+}
+
+int Run(HWND hwnd) {
+	MSG msg = {0};
+	while (msg.message != WM_QUIT) {
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		} else {
+			Render();
+		}
+	}
+	return static_cast<int>(msg.wParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	HWND hwnd = InitWindow(hInstance, nCmdShow);
+	if (!hwnd)
+		return -1;
+
+	if (FAILED(InitDirect3D(hwnd)))
+		return -1;
+
+	return Run(hwnd);
 }
